830b1fffa7ca99899f7f5790041e0198235fffaf
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2020-01-27 11:50:42 -0600

    Finished script to add new contacts
    Signed-off-by: Eddie Federmeyer <eddiefedermeyer@yahoo.com>

--- new_contact.py
+++ new_contact.py
@@ -6,17 +6,38 @@ Copyright 2020 Eddie Federmeyer
 """
 import json
 
-try:
-    with open("phone.json", "r+") as file:
-        tmp_json: dict = json.load(file)
 
-        name: str = input("Name: ")
-        number: str = input("Number (Format - xxxyyyzzz): ")
-        carrier: str = input("carrier (Options - att, tmobile, boost, metro, sprint, verizon, cricket): ")
+def __main__():
+    try:
 
-        tmp_json["contacts"][name] = {"phone": number, "carrier": carrier}
+        with open("phone.json", "r+") as file:
+            tmp_json: dict = json.load(file)
 
-        file.write(json.loads(tmp_json), file)
+            name: str = input("Name: ")
+            while True:
+                number: str = input("Number (Format - xxxyyyzzzz): ")
+                if number.isnumeric() and len(number) == 10:
+                    break
+                else:
+                    print("Either too long or not a number! Try again!")
+                    pass
 
-except IOError:
-    print("File does not exist")
+            while True:
+                carrier: str = input("carrier (Options - att, tmobile, boost, metro, sprint, verizon, cricket): ")
+                if tmp_json["carriers"].keys().__contains__(carrier):
+                    break
+                else:
+                    print("Not a valid carrier! Try again!")
+                    pass
+
+            tmp_json["contacts"][name] = {"phone": number, "carrier": carrier}
+
+            file.seek(0)
+            file.write(json.dumps(tmp_json, indent=4, sort_keys=True))
+            file.close()
+
+    except IOError:
+        print("File does not exist, did you delete it?")
+
+
+__main__()

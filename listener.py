4718ba9c959e163434bc24887e659ec9e071a013
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2020-05-21 01:31:41 -0500

    ~ Cleaned up listener.py ~ Changed phone.json contacts into a list, rather than a dictionary ~ Created settings file to be stored privately for login ~ Created a test phone list to use in development
    ** Note that the phone.json will be added to the .gitignore next commit so I can be used when deployed without sharing personal phone information.

--- listener.py
+++ listener.py
@@ -1,10 +1,9 @@
 # coding=utf-8
 """
-This is a simple project made to alert me any time a new game is available for free
+    This is a simple project made to alert me any time a new game is available for free
+    Data is sourced by www.indiegamebundles.com
 
-Data is sourced by indiegamebundles.com
-
-Copyright 2019 Eddie Federmeyer
+    Copyright 2019 Eddie Federmeyer
 """
 import json
 import random
@@ -16,59 +15,46 @@ from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# Load settings // Replace with enviroment var refs
+#
+# If getting errors with authentication and using gmail.
+# visit https://myaccount.google.com/lesssecureapps to unlock email
+settings = json.load(open('./settings.json', 'r+'))
+smtp_server = settings['smtp server'] or None
+from_addr = settings['email'] or None
+password = settings['password'] or None
 
-smtp_server: str = 'smtp.gmail.com'
-phrases: list = ['Guess what? Free Game!', 'Here\'s a free game for you!', 'Enjoy a free game!', 'Surprise!']
-faces: list = ['ᕕ(⌐■_■)ᕗ ♪♬', '╰(✿˙ᗜ˙)੭━☆ﾟ.*･｡ﾟ', '_|___|_  ╰(º o º╰)', 'ᕕ( ・‿・)つ-●-●']
-
-from_addr: str = 'alert.free.games@gmail.com'
-recipients: list = ['eddie']
-
-
-def send_mail(rec: str, t: str) -> None:
-    """
-    :param rec: Email of the recipient
-    :param t: The name of the game
-    Sends email through mms gateway
-    """
-
-    server = smtplib.SMTP(host=smtp_server, port=587)
-    server.starttls()
-    server.login(from_addr, '<password>')
-    server.sendmail(from_addr, rec, t)
-
-    print('Mail sent to "', rec, '"!')
-    return None
+# Message Decor
+phrases = ['Guess what? Free Game!', 'Here\'s a free game for you!', 'Enjoy a free game!', 'Surprise!']
+faces = ['ᕕ(⌐■_■)ᕗ ♪♬', '╰(✿˙ᗜ˙)੭━☆ﾟ.*･｡ﾟ', '_|___|_  ╰(º o º╰)', 'ᕕ( ・‿・)つ-●-●']
 
 
-def formulate_mail(g: str, li: str, rec: str, i: str) -> str:
-    """
-        :param g: Game
-        :param li: Link
-        :param rec: Recipient
-        :param i: Image
-        Builds an MIME email
-    """
-
+def send_mail(game: str, link: str, recipient: str, image_link: str) -> None:
     message = MIMEMultipart()
     message['FROM'] = from_addr
-    message['TO'] = rec
+    message['TO'] = recipient
     message['SUBJECT'] = random.choice(phrases)
-    message.attach(MIMEText(random.choice(faces) + '\n.\n' + g + '\n' + li, 'plain'))
+    message.attach(MIMEText(random.choice(faces) + '\n.\n' + game + '\n' + link, 'plain'))
 
     # Open image in binary mode
-    with requests.get(i) as attachment:
+    with requests.get(image_link) as attachment:
         part = MIMEImage(attachment.content)
         message.attach(part)
 
-    return message.as_string()
+    server = smtplib.SMTP(host=smtp_server, port=587)
+    server.starttls()
+    server.login(from_addr, password)
+    server.sendmail(from_addr, recipient, message.as_string())
+
+    print('Mail sent to "', recipient, '"!')
+    return None
 
 
-def __main__(app_path):
+def __main__():
     # Sends a request for the entire page on IndieGameBundles
-    r = requests.get('https://www.indiegamebundles.com/category/free/')
-    r = r.text
-    soup = BeautifulSoup(r, 'html.parser')
+    res = requests.get('https://www.indiegamebundles.com/category/free/')
+    res = res.text
+    soup = BeautifulSoup(res, 'html.parser')
 
     # Grabs the specific container that contains the latest post on the site
     game_data = soup.find(class_='entry-title td-module-title')
@@ -77,29 +63,36 @@ def __main__(app_path):
     image_link = soup.find(class_='td-image-wrap').img['data-img-url']
 
     # We will use a text file to store the most recent article title on the website
-    latest = open(app_path + '/latest.txt', 'r+')
-
-    # We will get phone data from phone.json
-    # And since Emails must be sent one at a time because T-Mobile is a bitch I gotta use a loop
-    # If the latest title received from the site doesn't match the title stored,
-    # that means there is a new game available!
-    phone_json: dict = json.load(open(app_path + '/phone.json', 'r'))
-
-    if len(phone_json['contacts']) == 0:
-        print('Please add a contact before running')
-        return None
-
+    latest = open('./latest.txt', 'r+')
+    # Double checks that the game is different then the previous free game
     if latest.read() != game:
-        for name in phone_json['contacts'].keys():
-
-            # Formats recipient address
-            recipient: str = phone_json['contacts'][name]['phone'] + '@' + \
-                        phone_json['carriers'][phone_json['contacts'][name]['carrier']]
-            body_text = formulate_mail(g=game, li=link, rec=recipient, i=image_link)
-            send_mail(rec=recipient, t=body_text)
-
-        # print('Mail sent!')
+        # Write the new game to the file so I don't forget...
         latest.write(game)
         latest.close()
+
+        # We will get phone data from phone.json
+        # And since Emails must be sent one at a time because T-Mobile is a bitch I gotta use a loop
+        # If the latest title received from the site doesn't match the title stored,
+        # That means there is a new game available!
+        phone_json = json.load(open('./phone.json', 'r'))
+
+        # Double checks if any contacts exist
+        if len(phone_json['contacts']) == 0:
+            print('Please add a contact before running')
+            return
+
+        # For every contact, format sms gateway
+        for contact in phone_json['contacts']:
+            # Formats recipient address
+            recipient = contact['phone'] + '@' + phone_json['carriers'][contact['carrier']]
+            # body_text = formulate_mail(g=game, li=link, rec=recipient, i=image_link)
+            # send_mail(rec=recipient, t=body_text)
+            send_mail(game=game, link=link, recipient=recipient, image_link=image_link)
+
+        print('Mailing list finished!')
+
     else:
         print('No new game!')
+
+
+__main__()
